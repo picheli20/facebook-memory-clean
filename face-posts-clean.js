6799cffef6365709cb8f7a9670c644488e2e3df4
Muka Schultze <samuelschultze@hotmail.com>
2018-08-03 21:44:44 -0300

    Fix: Keep at least 3 divs, don't destroy newest post

--- face-posts-clean.js
+++ face-posts-clean.js
@@ -1,14 +1,17 @@
 //Author Paulo Henrique
 //Clean Memory Facebook
-console.log("Facebook Memory Clean Started!")
+
+console.log("Facebook Memory Clean Started!");
 
 const clearMemory = () => {
   setTimeout(() => {
+    console.log("Cleaning!");
     Array.from(document.querySelectorAll('div'))
-    .filter(x => x.getAttribute('class') === '_4ikz')
-    .filter((x, idx) => idx <= 10)
-    .forEach(x => x.remove())
-    clearMemory()
+      .filter(x => x.getAttribute('class') === '_4ikz')
+      .filter((x, idx, items) => idx <= items.length - 3)
+      .forEach(x => x.remove());
+    clearMemory();
   }, 60000)
 }
-clearMemory()
+
+clearMemory();
