0a4d549bd73fec3b27cb347d2a059a2e671a777c
Fabio Picheli <contato@fabiopicheli.com>
2018-08-04 15:39:43 +0200

    Update face-posts-clean.js

--- face-posts-clean.js
+++ face-posts-clean.js
@@ -2,18 +2,16 @@
 //Clean Memory Facebook
 
 console.log("Facebook Memory Clean Started!");
-conat intervalKey;
+const intervalKey;
 
 const stopMemotyCleaning = () => clearInterval(intervalKey);
 
 const clearMemory = () => {
-  intervalKey = setInterval(() => {
-    console.log("Cleaning!");
-    Array.from(document.querySelectorAll('div'))
-      .filter(x => x.getAttribute('class') === '_4ikz')
-      .filter((x, idx, items) => idx <= items.length - 3)
-      .forEach(x => x.remove());
-  }, 60000)
+  console.log("Cleaning!");
+  Array.from(document.querySelectorAll('div'))
+    .filter(x => x.getAttribute('class') === '_4ikz')
+    .filter((x, idx, items) => idx <= items.length - 3)
+    .forEach(x => x.remove());
 }
 
-clearMemory();
+intervalKey = setInterval(clearMemory, 60000);
